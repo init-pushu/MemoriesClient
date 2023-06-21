3a7a39a2252516608dfa2d940687b4fda5ddff3a
init-pushu <115545185+init-pushu@users.noreply.github.com>
2023-06-21 22:45:46 +0530

    Update Auth.js

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -90,7 +90,7 @@ const SignUp = () => {
             { isSignup ? 'Sign Up' : 'Sign In' }
           </Button>
           <GoogleLogin
-            clientId="564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com"
+            clientId="616989950201-66dp204qe9m698kvictsmbdlihkkornt.apps.googleusercontent.com"
             render={(renderProps) => (
               <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                 Google Sign In
